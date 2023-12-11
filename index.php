65e2749952a46d63073ab1d838453577c1eb7bd6
rodion.karimov <rodion.karimov@tattelecom.digital>
2023-12-11 13:52:58 +0300

    TST-2 | Запуск приложения, инициализация маршрутов

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
 <?php
+
+
+declare(strict_types=1);
+
+namespace app;
+
+use app\controllers\ConvertController;
+use app\controllers\LoginController;
+use app\controllers\RegistrationController;
+use app\core\Application;
+
+require_once __DIR__ . "/vendor/autoload.php";
+(new ConfigParser(__DIR__ . '/env.json'))->load();
+
+if (getenv('APP_ENV') === 'dev') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('log_errors', 1);
+    ini_set('error_log', __DIR__ . getenv('FILE_LOG'));
+}
+
+$app = new Application();
+
+// Here is created a mapping of paths and files to open.
+$app->router->setGetPath('/', 'welcome');
+$app->router->setGetPath('/registration', [new RegistrationController(), 'registrationPage']);
+$app->router->setPostPath('/registrationController', [new RegistrationController(), 'registration']);
+$app->router->setGetPath('/404', [new NotFoundController(), 'notFound']);
+$app->router->setGetPath('/login', [new LoginController(), 'loginPage']);
+$app->router->setPostPath('/loginController', [new LoginController(), 'login']);
+$app->router->setGetPath('/converter', [new ConvertController(), 'converterPage']);
+$app->router->setPostPath('/convert', [new ConvertController(), 'convert']);
+
+$app->run();
